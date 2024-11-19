28473b46892ebfcf4f1bc3fa92e35b085fa265b6
Rubens Albukrk <rubensf4rda@gmail.com>
2024-11-19 18:05:45 -0300

    change style component input and menu compact

--- src/components/MenuCompact/MenuCompact.tsx
+++ src/components/MenuCompact/MenuCompact.tsx
@@ -13,45 +13,44 @@ const CompactMenu = () => {
   };
 
   return (
-    <div className="relative z-30">
+    <div className="relative z-30 p-0 m-0 items-center">
       <button
         onClick={toggleMenu}
-        className="text-white mt-2 mr-2 focus:outline-none"
+        className="text-white mt-3 mr-2 focus:outline-none"
       >
         {isOpen ? (
-          <div className="flex w-9 h-9 mb-2 rounded-full justify-center items-center bg-blue-500">
-            <FaTimes size={26} color="white" />
-          </div>
+          null
         ) : (
           <FaBars size={30} color={colors.sky[500]} className="mb-1" />
         )}
       </button>
 
-      <nav
-        className={`w-72 h-screen absolute top-0 right-16 bg-white p-4 transition-transform duration-300 ${
-          isOpen ? "transform scale-100" : "transform scale-0"
-        } origin-top-right md:hidden rounded-lg`}
+
+      <div
+        className={`absolute w-screen h-screen top-0 -right-0 bg-stone-900 p-4 transition-transform duration-300 ${
+          isOpen ? "transform scale-100 transition-all duration-500 ease-out" : "transform scale-0 transition-all duration-500 ease-out"
+        } bg-stone-900/95 origin-left md:hidden`}
       >
-        <div className="flex w-full h-20 bg-blue-400 rounded-lg mb-8 items-center justify-center">
-          <h1 className="text-white font-extrabold text-3xl">MENU</h1>
-        </div>
-        <ul className="space-y-4 font-bold">
+        <button onClick={toggleMenu} className="absolute right-4 flex w-9 h-9 mb-2 rounded-full justify-center items-center bg-blue-500">
+          <FaTimes size={26} color="white" />
+        </button>
+        <nav className='absolute w-7/12 mt-20 '>
+        <ul className="text-start space-y-4 font-bold">
           <li>
-              <p onClick={() => navigation('/')} className="text-sky-600 text-2xl hover:text-gray-300">
+              <p onClick={() => navigation('/')} className="text-white border-b-2 border-white text-2xl inline-block hover:text-gray-300">
                 Inicio
               </p>
 
           </li>
           <li>
 
-              <p onClick={() => navigation('/services')} className="text-sky-600 text-2xl hover:text-gray-300">
+              <p onClick={() => navigation('/services')} className="text-white border-b-2 border-white text-2xl inline-block hover:text-gray-300">
                 Serviços
               </p>
 
           </li>
           <li>
-            
-              <p onClick={() => navigation('/about')} className="text-sky-600 text-2xl hover:text-gray-300">
+              <p onClick={() => navigation('/about')} className="text-white border-b-2 border-white text-2xl inline-block hover:text-gray-300">
                 Sobre nós
               </p>
           </li>
@@ -60,20 +59,21 @@ const CompactMenu = () => {
           <li>
             <p
               onClick={() => window.open("https://wa.me/5583987904804")}
-              className="text-sky-600 text-2xl hover:text-gray-300"
-            >
+              className="text-white border-b-2 border-white text-2xl inline-block hover:text-gray-300">
               Contato
             </p>
           </li>
           <li>
 
-              <p onClick={() => navigation('/auth')} className="text-sky-600 mt-10 text-2xl hover:text-gray-300">
+              <p onClick={() => navigation('/auth')} className="text-white border-b-2 border-white text-2xl inline-block hover:text-gray-300">
                 Login
               </p>
 
           </li>
         </ul>
-      </nav>
+        </nav>
+        
+      </div>
     </div>
   );
 };
