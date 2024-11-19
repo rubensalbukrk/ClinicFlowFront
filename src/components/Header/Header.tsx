1e8472ad0bd397f0963bd1b68df0f3a8b4486c63
Rubens Albukrk <dev-rubens@Rubenss-MacBook-Pro.local>
2024-11-19 17:48:43 -0300

    input change style and menu compact

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -12,11 +12,19 @@ const Header = () => {
     <header>
       {/**MENU COMPACTO */}
       <nav
+<<<<<<< HEAD
         className="fixed flex flex-row z-50 h-16 top-0 left-0 w-screen px-2 items-center justify-between sm:hidden
                 bg-white/30 backdrop-blur-md shadow-black/10 shadow-md"
       >
         <img src={Logo} height={32} width={32} alt='logo' />
         <h1 className="bebas-font-regular mr-2 text-2xl text-sky-600">Clínica Odontológica</h1>
+=======
+        className="fixed flex flex-row z-50 h-16 p-0 top-0 left-0 w-screen justify-between sm:hidden
+                bg-white/30 backdrop-blur-md shadow-black/10 shadow-md"
+      >
+        <img src={Logo} height={32} width={32} alt='logo' className="self-center" />
+        <h1 className="bebas-font-regular mr-2 self-center text-2xl text-sky-600">Clínica Odontológica</h1>
+>>>>>>> 983e0d7 (change input colors and border)
         <CompactMenu />
         {/* <Logo className="flex ml-4 object-fill mr-3" /> */}
       </nav>
--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -2,7 +2,20 @@ import React from "react";
 import { InputProps } from "./type";
 import InputIcon from "./InputIcon/InputIcon";
 
+<<<<<<< HEAD
 const Input: React.FC<InputProps> = ({ id, children, asWarning, className, icon, ...props }) => {
+=======
+const Input: React.FC<InputProps> = (
+  { 
+    id, 
+    children, 
+    asWarning, 
+    className,
+    icon, 
+    ...props 
+  }
+) => {
+>>>>>>> 983e0d7 (change input colors and border)
   return (
     <div className={`flex flex-col my-2 items-start relative ${asWarning ? "mb-8" : "mb-0"}`}>
       {/* `relative` para permitir posicionamento absoluto */}
@@ -14,7 +27,12 @@ const Input: React.FC<InputProps> = ({ id, children, asWarning, className, icon,
           key={id}
           id={id}
           {...props}
+<<<<<<< HEAD
           className={`${className} ` +  `w-80 pl-10 pb-1 pt-2 border rounded-md ${icon ? "px-9" : "px-2"}`}
+=======
+          
+          className={`${className} ` +  `w-80 pl-10 pb-1 pt-2 placeholder-gray-500/40 ring-1 ring-sky-400/20 ring-offset-2 focus:outline-none focus:ring-sky-600 rounded-md ${icon ? "px-9" : "px-2"}`}
+>>>>>>> 983e0d7 (change input colors and border)
         />
     </div>
   );
