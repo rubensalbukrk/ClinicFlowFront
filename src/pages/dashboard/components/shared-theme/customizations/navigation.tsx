c0f8bab497d269baed3080274fc4e5e9be5d0653
Rubens Albukrk <rubensf4rda@gmail.com>
2024-12-09 15:04:28 -0300

    fix vars in navigation

--- src/pages/dashboard/components/shared-theme/customizations/navigation.tsx
+++ src/pages/dashboard/components/shared-theme/customizations/navigation.tsx
@@ -101,7 +101,7 @@ export const navigationCustomizations: Components<Theme> = {
           },
         }),
       }),
-      select: ({ theme }) => ({
+      select: ({ theme }: {theme: any}) => ({
         display: 'flex',
         alignItems: 'center',
         ...theme.applyStyles('dark', {
@@ -119,7 +119,7 @@ export const navigationCustomizations: Components<Theme> = {
       underline: 'none',
     },
     styleOverrides: {
-      root: ({ theme }) => ({
+      root: ({ theme }: {theme: any}) => ({
         color: (theme.vars || theme).palette.text.primary,
         fontWeight: 500,
         position: 'relative',
@@ -149,14 +149,14 @@ export const navigationCustomizations: Components<Theme> = {
   },
   MuiDrawer: {
     styleOverrides: {
-      paper: ({ theme }) => ({
+      paper: ({ theme }: {theme: any}) => ({
         backgroundColor: (theme.vars || theme).palette.background.default,
       }),
     },
   },
   MuiPaginationItem: {
     styleOverrides: {
-      root: ({ theme }) => ({
+      root: ({ theme }: {theme: any}) => ({
         '&.Mui-selected': {
           color: 'white',
           backgroundColor: (theme.vars || theme).palette.grey[900],
@@ -173,7 +173,7 @@ export const navigationCustomizations: Components<Theme> = {
   MuiTabs: {
     styleOverrides: {
       root: { minHeight: 'fit-content' },
-      indicator: ({ theme }) => ({
+      indicator: ({ theme }: {theme: any}) => ({
         backgroundColor: (theme.vars || theme).palette.grey[800],
         ...theme.applyStyles('dark', {
           backgroundColor: (theme.vars || theme).palette.grey[200],
@@ -183,7 +183,7 @@ export const navigationCustomizations: Components<Theme> = {
   },
   MuiTab: {
     styleOverrides: {
-      root: ({ theme }) => ({
+      root: ({ theme }: {theme: any}) => ({
         padding: '6px 8px',
         marginBottom: '8px',
         textTransform: 'none',
@@ -216,7 +216,7 @@ export const navigationCustomizations: Components<Theme> = {
   },
   MuiStepConnector: {
     styleOverrides: {
-      line: ({ theme }) => ({
+      line: ({ theme }: {theme: any}) => ({
         borderTop: '1px solid',
         borderColor: (theme.vars || theme).palette.divider,
         flex: 1,
@@ -226,7 +226,7 @@ export const navigationCustomizations: Components<Theme> = {
   },
   MuiStepIcon: {
     styleOverrides: {
-      root: ({ theme }) => ({
+      root: ({ theme }: {theme: any}) => ({
         color: 'transparent',
         border: `1px solid ${gray[400]}`,
         width: 12,
@@ -268,7 +268,7 @@ export const navigationCustomizations: Components<Theme> = {
   },
   MuiStepLabel: {
     styleOverrides: {
-      label: ({ theme }) => ({
+      label: ({ theme }: {theme: any}) => ({
         '&.Mui-completed': {
           opacity: 0.6,
           ...theme.applyStyles('dark', { opacity: 0.5 }),
