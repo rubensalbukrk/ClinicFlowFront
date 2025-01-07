c8867b3f5fddad84d11c73e9d1b0ed036cf217d6
Rubens Albukrk <rubensf4rda@gmail.com>
2025-01-07 20:34:32 -0300

    fixed call api await func

--- src/pages/dashboard/components/appointments/components/CustomBasicLayout/http/allPacients.ts
+++ src/pages/dashboard/components/appointments/components/CustomBasicLayout/http/allPacients.ts
@@ -2,9 +2,14 @@ import PacientService from "src/services/http/HttpPacients";
 
 const pacientServices = new PacientService();
 
-const allPacients = await pacientServices.GET();
+const GetAllPacients = async () => {
+  const pacients = await pacientServices.GET();
+  return pacients
+}
 
-const PACIENTS_DATA = allPacients.map((pacient) => ({
+const allPacients = await GetAllPacients()
+
+const PACIENTS_DATA = allPacients.map((pacient: any) => ({
   pacientId: pacient.pacientId,
   name: pacient.name
 }))
--- src/pages/dashboard/components/appointments/components/CustomBasicLayout/http/allProfessionals.ts
+++ src/pages/dashboard/components/appointments/components/CustomBasicLayout/http/allProfessionals.ts
@@ -2,7 +2,13 @@ import UserService from "src/services/http/HttpUsers";
 
 const userService = new UserService();
 
-const allUsers = await userService.GET();
+const GetAllUsers = async () => {
+  const users = await userService.GET();
+  return users
+}
+
+const allUsers = await GetAllUsers()
+
 
 const PROFESSIONALS_DATA = allUsers.map((user) => ({
   professionalId: user.professionalId,
